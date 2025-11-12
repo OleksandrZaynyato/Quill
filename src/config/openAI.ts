86412a665df76a6b8478f10439dd11acfeb6ff06
OleksandrZaynyato <153942558+OleksandrZaynyato@users.noreply.github.com>
2025-11-12 22:03:22 +0200

    clear key 2

--- src/config/openAI.ts
+++ src/config/openAI.ts
@@ -1,16 +1,16 @@
-import OpenAI from "openai";
-
-const openai = new OpenAI({
-    apiKey: "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",
-});
-
-const response = openai.responses.create({
-    model: "gpt-5-nano",
-    input: "write a haiku about ai",
-    store: true,
-});
-
-response.then((result) => console.log(result.output_text));
+// import OpenAI from "openai";
+//
+// const openai = new OpenAI({
+//     apiKey: "",
+// });
+//
+// const response = openai.responses.create({
+//     model: "gpt-5-nano",
+//     input: "write a haiku about ai",
+//     store: true,
+// });
+//
+// response.then((result) => console.log(result.output_text));
 
 //curl https://api.openai.com/v1/responses \
 //   -H "Content-Type: application/json" \
